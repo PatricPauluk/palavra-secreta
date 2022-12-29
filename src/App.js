fc1383060b0f5e5653bd3f9ea0d6820ac0cdfa3d
Patric Pauluk <patric.pauluk@outlook.com>
2022-12-28 22:16:22 -0300

    random categories and words

--- src/App.js
+++ src/App.js
@@ -25,8 +25,39 @@ function App() {
   const [ gameStage, setGameStage ] = useState(stages[0].name);
   const [ words ] = useState(wordsList);
 
+  const [ pickedWord, setPickedWord ] = useState('');
+  const [ pickedCategory, setPickedCategory ] = useState('');
+  const [ letters, setLetters ] = useState([]);
+
+
+  // escolhe a palavra e a categoria
+  const pickedWordAndCategory = () => {
+    // captura as keys (nomes) de cada array dentro do objeto
+    const categories = Object.keys(words);
+
+    // captura uma categoria aleatória (conforme a quantidade de keys do objeto) .floor arredonda o float
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    // captura uma palavra aleatória dentro da categoria (conforme a quantidade de palavras)
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    // retorna a palavra e a categoria aleatória desestruturado como um objeto
+    return { word, category };
+  }
+
   // inicia o jogo
   const startGame = () => {
+    // captura a palavra e categoria aleatória
+    const { word, category } = pickedWordAndCategory()
+
+    // cria um array de letras com a palavra aleatória e em minúsculo
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map(l => l.toLowerCase())
+
+    // insere nos estados
+    setPickedCategory(category);
+    setPickedWord(word);
+    setLetters(wordLetters)
+
     setGameStage(stages[1].name);
   }
   
